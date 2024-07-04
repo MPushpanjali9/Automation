8320afd9bb4b5482996fcce9f02a6842471504b4
Pushpanjali_G <Pushpanjali_g@epam.com>
2024-07-05 00:42:28 +0530

    my app commit2

--- src/test/java/com/example/App.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Parameter value: " + System.getProperty("param"));
-    }
-}
