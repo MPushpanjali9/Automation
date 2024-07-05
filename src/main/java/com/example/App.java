9418c146fb8f9e68a2692ccfafa722edc67d0535
Pushpanjali_G <Pushpanjali_g@epam.com>
2024-07-05 13:46:53 +0530

    myapp1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("Parameter value: " + System.getProperty("param"));
+		System.out.println("Parameter value: " + args[0]);
 	}
 }
